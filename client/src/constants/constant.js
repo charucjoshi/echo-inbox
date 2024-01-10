cb73c7904479ade85e51a8f56408a9b2d54f07c7
charucjoshi <charu.ccj@gmail.com>
2024-01-10 13:45:33 +0530

    Completed

--- client/src/constants/constant.js
+++
@@ -1,2 +0,0 @@
-//Logo Name = echoInboxLogo
-export const echoInboxLogo = 'https://ibb.co/0n8yBMG';
--- server/routes/route.js
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-import {saveSentEmails} from '../controller/email-controller.js';
-
-const routes = express.Router();
-
-routes.post('/save', saveSentEmails);
-
-export default routes;
